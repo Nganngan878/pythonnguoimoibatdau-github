70fc350e2e832d527c9960922b3be57e9771db03
Ngan Phan <samsungcuakhach0507@gmail.com>
2026-05-22 00:08:21 +0700

    hoàn  thanh bai tap c6

---
+++ python.py/c6.py/SRAEquality.py
@@ -0,0 +1,17 @@
+x=99;
+print(x);
+x='python';
+print(x);
+L=[1,2,3]
+print(L);
+M=L;
+print(M);
+L==M;
+print(L==M);
+L is M;
+print(L is M);
+import sys;
+sys.getrefcount(99);
+print(sys.getrefcount(99));
+print(sys.getrefcount(2 ** 10000));
+
---
+++ python.py/c6.py/c6TypeHinting.py
@@ -0,0 +1,15 @@
+from typing import List
+
+# 1. Khởi tạo biến kèm kiểu dữ liệu
+a: int = 10
+b: int = 0
+c: List[int] = [1, 2, 3]
+Data = List[float]
+
+# 3. Thực thi
+print(a)
+b = 'hakc'   
+print(b)
+c = 'code'     
+print(c)
+print(b, c)
